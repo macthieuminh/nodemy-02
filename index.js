ac58f4700bc3e5f1a9b1cb7a2c8c3a44fac77364
TM_ <35858330+macthieuminh@users.noreply.github.com>
2023-02-08 16:19:44 +0700

    update text / image

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function setShow () {
+const modal = document.querySelector(".modal");
+function openModal() {
+    modal.classList.add("open");
 }
+function closeModal() {
+    modal.classList.remove("open");
+}
+modal.addEventListener("click", closeModal);
